ba3256d1e1c3365d6ce4e31355dca6a68effa057
zwirok-git <oleksijprokopenko@gmail.com>
2026-07-02 10:51:03 +0300

    add global database settings

---
+++ src/database.py
@@ -0,0 +1,27 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from dotenv import load_dotenv
+from sqlalchemy.orm import DeclarativeBase
+
+from config import settings
+
+load_dotenv()
+
+engine = create_async_engine(
+    settings.database_url,
+    connect_args={"check_same_thread": False}
+    if "sqlite" in settings.database_url
+    else {},
+    echo=True,
+)
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine, autoflush=False, autocommit=False, expire_on_commit=False
+)
+
+
+async def get_db():
+    async with AsyncSessionLocal() as db:
+        yield db
+
+
+class Base(DeclarativeBase):
+    pass
