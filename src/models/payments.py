ae3417327380cd055edc984e601776d420f7184a
PavelPereverzev1 <pereverzev.pavel1975@gmail.com>
2026-07-03 12:47:09 +0300

    fix: fixed import path

--- src/models/payments.py
+++ src/models/payments.py
@@ -6,7 +6,7 @@ from typing import Optional
 from sqlalchemy import DECIMAL, DateTime, ForeignKey, String, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from src.database import Base
+from src.core.database import Base
 
 
 class PaymentStatus(str, Enum):
