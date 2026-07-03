34408275a274c023b1035185b50ca971b45e3554
PavelPereverzev1 <pereverzev.pavel1975@gmail.com>
2026-07-04 00:30:06 +0300

    fix: config.py

--- src/core/config.py
+++ src/core/config.py
@@ -1,5 +1,3 @@
-import os
-
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -9,11 +7,11 @@ class Settings(BaseSettings):
         extra="ignore",
     )
 
-    POSTGRES_DB: str = os.getenv("DB_USER", "test_user")
-    POSTGRES_PASSWORD: str = os.getenv("DB_PASSWORD", "test_password")
-    POSTGRES_USER: str = os.getenv("DB_HOST", "test_host")
-    POSTGRES_DB_PORT: int = int(os.getenv("DB_PORT", 5432))
-    POSTGRES_HOST: str = os.getenv("DB_NAME", "test_db")
+    POSTGRES_DB: str = "movies_db"
+    POSTGRES_PASSWORD: str = "some_password"
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_DB_PORT: int = 5432
+    POSTGRES_HOST: str = "localhost"
 
     @property
     def database_url(self) -> str:
@@ -22,7 +20,7 @@ class Settings(BaseSettings):
             f"@{self.POSTGRES_USER}:{self.POSTGRES_DB_PORT}/{self.POSTGRES_HOST}"
         )
 
-    STRIPE_SECRET_KEY: str
+    STRIPE_SECRET_KEY: str = "sk_test_placeholder"
     STRIPE_WEBHOOK_SECRET: str | None = None
 
 
