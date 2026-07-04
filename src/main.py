536ee19bd0215082eec89c0d3e75a6ddf2db009d
PavelPereverzev1 <pereverzev.pavel1975@gmail.com>
2026-07-04 13:42:21 +0300

    feat: Added base API structure

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 
+from api import api_v1_router
+from core.config import settings
+
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"Hello": "world!"}
+app.include_router(api_v1_router, prefix=settings.API_V1_PREFIX)
