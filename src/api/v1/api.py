0acfa0f0e07f42d8b365d0d55bf91c0ff636ca2c
PavelPereverzev1 <pereverzev.pavel1975@gmail.com>
2026-07-05 10:36:59 +0300

    fix: routers in api

--- src/api/v1/api.py
+++ src/api/v1/api.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
 from .payments import router as payments_router
+from .users import router as users_router
 
 
 api_router = APIRouter()
 
+api_router.include_router(users_router, prefix="/users", tags=["users"])
 api_router.include_router(payments_router, tags=["Payments"])
