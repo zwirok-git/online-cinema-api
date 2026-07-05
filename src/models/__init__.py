8ef09ba5d329cb58ba830bf6699be739ecf301f9
PavelPereverzev1 <pereverzev.pavel1975@gmail.com>
2026-07-05 10:32:06 +0300

    fix: __init__.py in models

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,51 @@
+from .movies import (
+    Certification,
+    CommentLikeModel,
+    CommentModel,
+    Director,
+    FavoriteModel,
+    Genre,
+    Movie,
+    MovieDirector,
+    MovieGenre,
+    MovieLikeModel,
+    MovieRatingModel,
+    MovieStar,
+    Star,
+)
+from .orders import Order, OrderItem
 from .payments import Payment, PaymentItem, PaymentStatus
-from .users import UserModel
+from .tokens import (
+    ActivationTokenModel,
+    PasswordResetTokenModel,
+    RefreshTokenModel,
+)
+from .users import UserGroupModel, UserModel, UserProfileModel
 
 
-__all__ = ["Payment", "PaymentItem", "PaymentStatus", "UserModel"]
+__all__ = [
+    "Payment",
+    "PaymentItem",
+    "PaymentStatus",
+    "UserModel",
+    "UserGroupModel",
+    "UserProfileModel",
+    "ActivationTokenModel",
+    "PasswordResetTokenModel",
+    "RefreshTokenModel",
+    "Order",
+    "OrderItem",
+    "Genre",
+    "Director",
+    "Star",
+    "Certification",
+    "MovieGenre",
+    "MovieDirector",
+    "MovieStar",
+    "Movie",
+    "MovieLikeModel",
+    "FavoriteModel",
+    "MovieRatingModel",
+    "CommentLikeModel",
+    "CommentModel",
+]
