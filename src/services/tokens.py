c087cb094aa0e4e28a85c3906328419429f656a9
andriihrechko <andriihrechko250703@gmail.com>
2026-07-06 19:00:30 +0300

    update tokens service

--- src/services/tokens.py
+++ src/services/tokens.py
@@ -2,15 +2,9 @@ import secrets
 from datetime import datetime, timedelta, timezone
 
 from core.config import settings
-from exceptions.auth import (
-    TokenAlreadyExists,
-    TokenExpired,
-    TokenInvalid,
-    TokenNotExists,
-)
-from models import RefreshTokenModel
+from exceptions.auth import TokenAlreadyExpired, TokenDoesNotExists
+from models import PasswordResetTokenModel, RefreshTokenModel
 from models.tokens import ActivationTokenModel
-from models.users import UserModel
 from repositories.tokens import TokenRepository
 
 
@@ -18,52 +12,93 @@ class TokenService:
     def __init__(self, token_repository: TokenRepository) -> None:
         self.token_repository = token_repository
 
-    async def get_token_by_token(
+    async def get_activation_token_by_token(
         self, token_value: str
     ) -> ActivationTokenModel | None:
-        token = await self.token_repository.get_activation_token(token_value)
+        token = await self.token_repository.get_activation_token_by_token(
+            token_value=token_value
+        )
         if token is None:
-            raise TokenNotExists("Invalid token.")
+            raise TokenDoesNotExists("Activation token does not exist.")
+        if token.expires_at < datetime.now(timezone.utc):
+            raise TokenAlreadyExpired("Activation token has already expired.")
+        return token
+
+    async def get_activation_token_by_user_id(
+        self, user_id: int
+    ) -> ActivationTokenModel | None:
+        token = await self.token_repository.get_activation_token_by_user_id(
+            user_id
+        )
         return token
 
     async def create_activation_token(
         self, user_id: int
     ) -> ActivationTokenModel:
-        exists_token = (
-            await self.token_repository.get_activation_token_by_user_id(
-                user_id=user_id
-            )
-        )
-        if exists_token is not None:
-            raise TokenAlreadyExists("Token already exists. Check your email.")
-
-        token = ActivationTokenModel(
+        activation_token = ActivationTokenModel(
             token=secrets.token_urlsafe(32),
             user_id=user_id,
             expires_at=datetime.now(timezone.utc) + timedelta(hours=24),
         )
-        await self.token_repository.create_activation_token(token)
+        await self.token_repository.create_activation_token(
+            token=activation_token
+        )
+        return activation_token
+
+    async def delete_activation_token(
+        self, token: ActivationTokenModel
+    ) -> None:
+        await self.token_repository.delete_activation_token(token)
+
+    async def get_reset_token_by_token(
+        self, token_value: str
+    ) -> PasswordResetTokenModel | None:
+        token = await self.token_repository.get_password_reset_token_by_token(
+            token_value=token_value
+        )
+        if token is None:
+            raise TokenDoesNotExists("Token with this value does not exist.")
+        if token.expires_at < datetime.now(timezone.utc):
+            raise TokenAlreadyExpired("Token has already expired.")
         return token
 
-    async def delete_activation_token(self, token_value: str) -> None:
-        token = await self.token_repository.get_activation_token(token_value)
+    async def get_reset_token_by_user_id(
+        self, user_id: int
+    ) -> PasswordResetTokenModel | None:
+        token = (
+            await self.token_repository.get_password_reset_token_by_user_id(
+                user_id=user_id
+            )
+        )
+        return token
+
+    async def create_reset_token(
+        self, user_id: int
+    ) -> PasswordResetTokenModel:
+        reset_token = PasswordResetTokenModel(
+            token=secrets.token_urlsafe(32),
+            user_id=user_id,
+            expires_at=datetime.now(timezone.utc) + timedelta(hours=24),
+        )
+        await self.token_repository.create_password_reset_token(
+            token=reset_token
+        )
+        return reset_token
+
+    async def delete_reset_token(self, token: ActivationTokenModel) -> None:
+        await self.token_repository.delete_password_reset_token(token)
+
+    async def get_refresh_token_by_token(
+        self, token_value: str
+    ) -> RefreshTokenModel | None:
+        token = await self.token_repository.get_refresh_token_by_token(
+            token_value=token_value
+        )
         if token is None:
-            raise TokenNotExists("Invalid token.")
-        await self.token_repository.delete_activation_token(token)
-
-    async def activate_user(self, token_value: str) -> UserModel:
-        token = await self.token_repository.get_activation_token(token_value)
-
-        if token is None:
-            raise TokenInvalid("Invalid token.")
-
+            raise TokenDoesNotExists("Token with this value does not exist.")
         if token.expires_at < datetime.now(timezone.utc):
-            await self.token_repository.delete_activation_token(token)
-            raise TokenExpired("Token expired. Check your email.")
-
-        token.user.is_active = True
-        await self.token_repository.delete_activation_token(token)
-        return token.user
+            raise TokenAlreadyExpired("Token has already expired.")
+        return token
 
     async def create_refresh_token(
         self, token_value: str, user_id: int
@@ -74,9 +109,16 @@ class TokenService:
             + settings.REFRESH_TOKEN_EXPIRE,
             token=token_value,
         )
-        return await self.token_repository.create_refresh_token(
-            token=refresh_token
-        )
+        await self.token_repository.create_refresh_token(token=refresh_token)
+        return refresh_token
 
-    async def delete_all_refresh_token(self, user_id: int) -> None:
-        await self.token_repository.delete_refresh_tokens(user_id=user_id)
+    async def delete_refresh_token(self, token_value: int) -> None:
+        token = await self.token_repository.get_refresh_token_by_token(
+            token_value=token_value
+        )
+        await self.token_repository.delete_refresh_token(token=token)
+
+    async def delete_all_refresh_tokens(self, user_id: int) -> None:
+        await self.token_repository.delete_users_refresh_tokens(
+            user_id=user_id
+        )
