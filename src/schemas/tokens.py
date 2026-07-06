6316ecaa8486f21f402e3d2c1152aa96938b62ac
andriihrechko <andriihrechko250703@gmail.com>
2026-07-06 18:58:40 +0300

    add token schema

--- src/schemas/tokens.py
+++ src/schemas/tokens.py
@@ -5,4 +5,9 @@ class TokenPairResponseSchema(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
     access_token: str
-    refresh: str
+    refresh_token: str
+    token_type: str = "bearer"
+
+
+class TokenRefreshRequestSchema(BaseModel):
+    refresh_token: str
